916c200a3378bdd223a8b67856863e3966ac536d
daphnalex <daphne.bordel@gmail.com>
2018-04-15 15:24:08 +0200

    correctif slider

--- src/app/page-one/page-one.component.ts
+++ src/app/page-one/page-one.component.ts
@@ -30,14 +30,16 @@ export class PageOneComponent implements OnInit {
          evt.preventDefault();
          // enregistre la valeur de l'attribut  href dans la variable target
   	var target = $(this).attr('href');
-         /* le sélecteur $(html, body) permet de corriger un bug sur chrome
-         et safari (webkit) */
-  	$('html, body')
-         // on arrête toutes les animations en cours
-         .stop()
-         /* on fait maintenant l'animation vers le haut (scrollTop) vers
-          notre ancre target */
-         .animate({scrollTop: $(target).offset().top}, 1000 );
+        if (target !== "#realisation"){
+             /* le sélecteur $(html, body) permet de corriger un bug sur chrome
+             et safari (webkit) */
+      	      $('html, body')
+               // on arrête toutes les animations en cours
+               .stop()
+               /* on fait maintenant l'animation vers le haut (scrollTop) vers
+                notre ancre target */
+               .animate({scrollTop: $(target).offset().top}, 1000 );
+          }
       });
     });
 
